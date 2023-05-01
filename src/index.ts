3670e347df6bf3a7876e1c04b886ce7304584633
Arnav-Jhajharia <jhajhariaarnav@gmail.com>
2023-05-01 20:53:05 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -292,7 +292,8 @@ async function main(): Promise<void> {
     // ...
 
     // Redirect to the app with the referral code as a query parameter
-    res.set('location', 'https://onelink.to/weupf9');
+        res.set('Content-Type', 'text/html');
+  res.redirect(301, 'https://onelink.to/weupf9');
     }
     catch (e) {
       res.send(500)
